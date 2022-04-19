c76453f83bbdf8625fbda3f8ce9ea7840507d328
David Clark <daviedwardclark@gmail.com>
2022-04-18 21:36:52 -0500

    many bugs fixed

---
+++ js/date.js
@@ -0,0 +1,7 @@
+(function() {
+
+  const currentYear = new Date().getFullYear();
+  const yearContainer = document.querySelector('time');
+  yearContainer.innerHTML = currentYear;
+
+})();
---
+++ js/hamburger.js
@@ -0,0 +1,25 @@
+(function() {
+
+  const screenMinWidth = window.matchMedia("(min-width: 577px)");
+  const hamburgerButton = document.getElementsByClassName("hamburger")[0];
+
+  hamburgerButton.onclick = function myFunction() {
+    let mobileLinks = document.getElementById('myMobileLinks');
+    if (mobileLinks.style.display === 'block') {
+      mobileLinks.style.display = 'none';
+    } else {
+      mobileLinks.style.display = 'block';
+    }
+  }
+
+  function myFunctionScreen(screenMinWidth) {
+    let mobileLinks = document.getElementById('myMobileLinks');
+    if (screenMinWidth.matches && mobileLinks.style.display === 'none') {
+      mobileLinks.style.display = 'block';
+    }
+  }
+
+  myFunctionScreen(screenMinWidth);
+  screenMinWidth.addListener(myFunctionScreen);
+
+})();
